10f03a18e93f6f4569a9fe2889b91542a51d01d8
Aitugan <aytugan242@gmail.com>
2024-05-04 10:52:22 +0300

    Initial commit

---
+++ logistic/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    product = ProductSerializer
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(stock_id=stock.id, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for position_data in positions_data:
+            obj, created = StockProduct.objects.filter(stock_id=stock.id).update_or_create(
+                stock=stock,
+                product=position_data['product'],
+                defaults={
+                    'quantity': position_data['quantity'],
+                    'price': position_data['price']
+                }
+            )
+
+        return stock
